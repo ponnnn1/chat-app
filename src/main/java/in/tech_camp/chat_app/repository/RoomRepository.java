c59022f49f4f1968cf86f1fbc0786203edcaabcf
ponnnn1 <miyawaki.ayana@minebea-ss.com>
2025-06-23 15:30:09 +0900

    チャット画面の編集

--- src/main/java/in/tech_camp/chat_app/repository/RoomRepository.java
+++ src/main/java/in/tech_camp/chat_app/repository/RoomRepository.java
@@ -3,6 +3,7 @@ package in.tech_camp.chat_app.repository;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Select;
 
 import in.tech_camp.chat_app.entity.RoomEntity;
 
@@ -12,4 +13,8 @@ public interface RoomRepository {
   @Insert("INSERT INTO rooms(name) VALUES(#{name})")
   @Options(useGeneratedKeys = true, keyProperty = "id")
   void insert(RoomEntity roomEntity);
+
+  // チャットルーム情報取得
+  @Select("SELECT * FROM rooms WHERE id = #{id}")
+  RoomEntity findById(Integer id);
 }
