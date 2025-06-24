f04e8addca5d0a753b3add39eaed697a88274367
ponnnn1 <miyawaki.ayana@minebea-ss.com>
2025-06-24 11:05:23 +0900

    メッセージ送信機能の実装

--- src/main/java/in/tech_camp/chat_app/controller/MessageController.java
+++ src/main/java/in/tech_camp/chat_app/controller/MessageController.java
@@ -7,11 +7,18 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import in.tech_camp.chat_app.custom_user.CustomUserDetail;
+import in.tech_camp.chat_app.entity.MessageEntity;
 import in.tech_camp.chat_app.entity.RoomEntity;
 import in.tech_camp.chat_app.entity.RoomUserEntity;
 import in.tech_camp.chat_app.entity.UserEntity;
+import in.tech_camp.chat_app.form.MessageForm;
+import in.tech_camp.chat_app.repository.MessageRepository;
+import in.tech_camp.chat_app.repository.RoomRepository;
 import in.tech_camp.chat_app.repository.RoomUserRepository;
 import in.tech_camp.chat_app.repository.UserRepository;
 import lombok.AllArgsConstructor;
@@ -24,9 +31,15 @@ public class MessageController {
 
   private final RoomUserRepository roomUserRepository;
 
+  private final RoomRepository roomRepository;
+
+  private final MessageRepository messageRepository;
+
   // @GetMapping("/")
-  @GetMapping("/message")
-  public String showMessages(@AuthenticationPrincipal CustomUserDetail currentUser, Model model){
+  // @GetMapping("/message")
+  // public String showMessages(@AuthenticationPrincipal CustomUserDetail currentUser, Model model){
+  @GetMapping("/rooms/{roomId}/messages")
+  public String showMessages(@PathVariable("roomId") Integer roomId,@AuthenticationPrincipal CustomUserDetail currentUser, Model model){
     UserEntity user = userRepository.findById(currentUser.getId());
     model.addAttribute("user", user);
     List<RoomUserEntity> roomUserEntities = roomUserRepository.findByUserId(currentUser.getId());
@@ -34,9 +47,31 @@ public class MessageController {
         .map(RoomUserEntity::getRoom)
         .collect(Collectors.toList());
     model.addAttribute("rooms", roomList);
+
+    // メッセージフォームの内容をビューに渡す
+    model.addAttribute("messageForm", new MessageForm());
+    model.addAttribute("roomId", roomId);
     return "messages/index";
   }
   // public String showMessages(){
   //     return "messages/index";
   // }
+
+  @PostMapping("/rooms/{roomId}/messages")
+  public String saveMessage(@PathVariable("roomId") Integer roomId, @ModelAttribute("messageForm") MessageForm messageForm, @AuthenticationPrincipal CustomUserDetail currentUser) {
+    MessageEntity message = new MessageEntity();
+    message.setContent(messageForm.getContent());
+
+    UserEntity user = userRepository.findById(currentUser.getId());
+    RoomEntity room = roomRepository.findById(roomId);
+    message.setUser(user);
+    message.setRoom(room);
+
+    try {
+      messageRepository.insert(message);
+    } catch (Exception e) {
+      System.out.println("エラー：" + e);
+    }
+    return "redirect:/rooms/" + roomId + "/messages";
+  }  
 }
